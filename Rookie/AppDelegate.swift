bba7482f5e5177719f48231c135cd9a55c7cc3c8
Elvis Tapfumanei <elvis@elmunei.net>
2016-11-23 10:09:13 +0200

    Trackers added

--- Rookie/AppDelegate.swift
+++ Rookie/AppDelegate.swift
@@ -7,7 +7,13 @@
 //
 
 import UIKit
+import Fabric
+import Optimizely
+import Branch
+import Crashlytics
 import CoreData
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +23,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self, Branch.self, Optimizely.self])
+        Optimizely.start(withAPIToken: "AANYCl0BrLYfnMYn72Bl4RaIzbSTSVqR~7899670464", launchOptions:launchOptions)
+
+        
         return true
     }
 
